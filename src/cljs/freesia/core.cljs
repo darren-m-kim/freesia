464d0d23057e4bb0cc4643b23f3e4fe5ff73fd29
Darren Minsoo Kim <darren.minsoo.kim@gmail.com>
2022-07-07 22:24:59 -0400

    implemented routing on frontend

--- src/cljs/freesia/core.cljs
+++ src/cljs/freesia/core.cljs
@@ -1,161 +1,69 @@
 (ns freesia.core
+  (:require-macros
+   [cljs.core.async.macros :refer [go]])
   (:require
-   [clojure.string :as cstr]
-   [reagent.core :as reag]
-   [reagent.dom :as rdom]
-   [freesia.state :as state]
-   [freesia.util :as util]
-   [freesia.element.sign.in :as sign-in]
-   [freesia.element.management.list :as mgmt-list]
-   [freesia.element.management.edit :as mgmt-edit]))
+   [cljs.core.async :refer [<!]]
+   [cljs-http.client :as http]
+   [reagent.core :as rc]
+   [reagent.dom :as rd]
+   [reitit.frontend :as rf]
+   [reitit.frontend.easy :as rfe]
+   [reitit.coercion.spec :as rcs]))
 
-(defn toggle-sidebar-btn []
-  [:button {:class "btn btn-action"
-            :type "button"}
-   [:i {:class "fa fa-bars"
-        :aria-hidden "true"}]])
+(def log (.-log js/console))
 
-(defn brand-logo []
-  [:a {:href "#"
-       :class "navbar-text text-monospace"}
-   [:p "bitem"]])
- 
-(defn change-color-btn []
-  (let [current-mode (-> @state/control :dark-mode)
-        flipping (fn []
-                   (swap! state/control assoc :dark-mode (not current-mode))
-                   (print @state/control))]
-    [:button {:class "btn btn-action"
-              :on-click flipping}
-     [:i {:class "fas fa-moon"
-          :aria-hidden "true"}]]))
+(defonce page (rc/atom nil))
 
-(defn search-box []
-  [:input {:type "text"
-           :class "form-control"
-           :placeholder "search"
-           :required "required"}])
+(defn current-page []
+  [:div
+   [:ul
+    [:li [:a {:href (rfe/href ::frontpage)} "Frontpage"]]
+    [:li [:a {:href (rfe/href ::about)} "About"]]
+   #_ [:li [:a {:href (rfe/href ::item {:id 1})} "Item 1"]]
+   #_ [:li [:a {:href (rfe/href ::item {:id 2} {:foo "bar"})} "Item 2"]]]
+   (when @page
+     (let [view (:view (:data @page))]
+       (print "@@@@@@")
+       (prn view)
+       (log view)
+       [view @page]))])
 
-(defn sign-change-btn []
-  [:button {:class "btn"
-            :on-click #(swap! state/control assoc :signed false)}
-   "Out"])
+(def tester (rc/atom "initialized."))
 
-(defn version-label []
-  [:span {:class "navbar-text text-monospace"}
-   (str "v" "0.1")])
+(defn ggg []
+  (go (let [response (<! (http/get "https://api.github.com/users"
+                                   {:with-credentials? false
+                                    :query-params {"since" 135}}))]
+        (prn (:status response))
+        (prn (map :login (:body response)))
+        (prn @tester)
+        (reset! tester (str "my love + " (:status response))))))
 
-(defn section-link [k]
-  [:li {:class (cstr/join " " ["nav-item"
-                               (when (= (-> @state/control :element first) k)
-                                 "active")])
-        :on-click #(swap! state/control assoc :element [k nil])}
-   [:a {:class "nav-link"} (-> k name cstr/capitalize)]])
+(defn home-page []
+  [:div
+   [:h2 "Welcome to frontend"]
+   [:button {:on-click ggg}]
+   [:p @tester]])
 
-(defn section-links []
-  [:ul {:class "navbar-nav d-none d-md-flex"}
-   (section-link :board)
-   (section-link :management)
-   (section-link :entity)
-   (section-link :account)])
+(defn about-page []
+  [:div
+   [:h2 "About frontend"]])
 
-(defn status []
-  (let [mgmt-name (or (-> @state/control :management)
-                      "Not Selected")
-        person-name (-> @state/control :person :name)]
-    [:span {:class "navbar-text"}
-     (str "[ " mgmt-name " - " person-name " ]")]))
-
-(defn nav-content []
-  [:div {:class "navbar-content"}
-   [toggle-sidebar-btn]
-   [brand-logo]
-   [version-label]
-   [section-links]
-   [status]])
-
-(defn nav-form []
-  [:form {:class "form-inline d-none d-md-flex ml-auto"}
-   [change-color-btn]
-   [search-box]
-   [sign-change-btn]])
-
-(defn navbar []
-  [:nav
-   {:class "navbar"}
-   [nav-content]
-   [nav-form]])
-
-(defn side-item [icon subject & add-fn]
-  (let [current-elem (-> @state/control :element)
-        new-elem [(first current-elem) subject]]
-    [:a (merge (util/class :sidebar-link
-                           :sidebar-link-with-icon
-                           :nav-item nil
-                           (when (= (second current-elem) subject) :text-success))
-               {:on-click (fn []
-                            (when add-fn ((first add-fn)))
-                            (swap! state/control assoc :element new-elem))})
-     [:span {:class "sidebar-icon"}
-      [:i {:class icon :aria-hidden true}]]
-     (-> subject name cstr/capitalize)]))
-
-(defn sidebar []
-  [:div {:class "sidebar"}
-   [:div {:class "sidebar-menu"}
-    [:br] [:br]
-    (case (-> @state/control :element first)
-      :board [:<>
-              (side-item "fa fa-terminal" "Entity")
-              (side-item "fa fa-terminal" "Ownership")
-              (side-item "fa fa-terminal" "Transaction")
-              (side-item "fa fa-terminal" "Journal")
-              (side-item "fa fa-terminal" "Ledger")
-              (side-item "fa fa-terminal" "Report")]
-      :management
-      [:<> 
-       (side-item "fa fa-terminal" :list)
-       (side-item "fa fa-terminal" :edit #(reset! mgmt-edit/entity (-> @state/control :management)))
-       (side-item "fa fa-terminal" :add)]
-      :entity [:<> 
-               (side-item "fa fa-terminal" "Entity")
-               (side-item "fa fa-terminal" "Ownership")
-               (side-item "fa fa-terminal" "Transaction")
-               (side-item "fa fa-terminal" "Journal")
-               (side-item "fa fa-terminal" "Ledger")
-               (side-item "fa fa-terminal" "Report")]
-      :account [:<> 
-                (side-item "fa fa-terminal" "Entity")
-                (side-item "fa fa-terminal" "Ownership")
-                (side-item "fa fa-terminal" "Transaction")
-                (side-item "fa fa-terminal" "Journal")
-                (side-item "fa fa-terminal" "Ledger")
-                (side-item "fa fa-terminal" "Report")])]])
-
-(defn element-section []
-  [:div {:class "content-wrapper"}
-   (case (-> @state/control :element)
-     [:management :list] [mgmt-list/table]
-     [:management :edit] [mgmt-edit/unit])])
-
-(defn app-pg []
-  [:div {:class "page-wrapper with-navbar with-sidebar with-navbar-fixed-bottom"}
-   [navbar]
-   [sidebar]
-   [element-section]])
-
-(defn scaffold
-  "the selector of all pages."
-  []
-  (if (:signed @state/control)
-    [app-pg]
-    [sign-in/page]))
+(def routes
+  [["/" {:name ::frontpage
+         :view home-page}]
+   ["/about" {:name ::about
+              :view about-page}]])
 
 (defn run []
-  (swap! state/control assoc :dark-mode true)
-  (print @state/control)
-  (rdom/render
-   [scaffold]
+   (rfe/start!
+    (rf/router routes {:data {:coercion rcs/coercion}})
+    (fn [m]
+      (prn "!!!!!!!" m)
+      (reset! page m))
+    {:use-fragment true})
+  (rd/render
+   [current-page]
    (js/document.getElementById "root")))
 
 (defn ^:export init []
